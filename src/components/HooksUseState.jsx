5b1ed44f3eda853599b6cd78e73954c64dec455c
Anup Tachamo <anuptachamo@gmail.com>
2023-09-14 21:27:25 +0545

    learning

--- src/components/HooksUseState.jsx
+++ src/components/HooksUseState.jsx
@@ -2,17 +2,22 @@ import React, { useState } from "react";
 import "../styles/HooksUseState.css";
 
 const HooksUseState = () => {
-  const [numbers, setNumbers] = useState(100);
+  const [add, setAdds] = useState(100);
+  const [subtract, setSubtract] = useState(100);
   return (
     <div>
-      <h1>Numbers: {numbers}</h1>
+      <h1>Numbers: {add} </h1>
       <button
         onClick={() => {
-          setNumbers(3000);
+          setAdds(add +100);
         }}
       >
-        click Here
+        Add
       </button>
+      <h1>{subtract}</h1>
+      <button onClick={()=>{
+        setSubtract(subtract -100);
+      }}>Subtract</button>
     </div>
   );
 };
